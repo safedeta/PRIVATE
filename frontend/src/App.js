38a623a23c0d480d18ea48bc12459baf4055d776
safedeta <jctama2002@gmail.com>
2024-12-06 14:07:36 -0500

    01

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header.jsx';
+import Navbar from './components/navbar.jsx';
+import Home from './components/home.jsx';
+import Footer from './components/footer.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header Component */}
+      <Header />
+      
+      {/* Navbar Component */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main>
+        <Home />
+      </main>
+
+      {/* Footer Component */}
+      <Footer />
     </div>
   );
 }
